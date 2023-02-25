1524d2562c49f96db608624ce4739f70d0b35487
Stefan Lindecke <stefan@chektrion.de>
2023-02-25 14:47:15 +0100

    Code cleanup und Umbau

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace lindesbs\toolbox\ContaoManager;
 
-use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
 use lindesbs\toolbox\ToolboxBundle;
 
 class Plugin implements BundlePluginInterface
@@ -19,7 +19,7 @@ class Plugin implements BundlePluginInterface
     {
         return [
             BundleConfig::create(ToolboxBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class]),
+                ->setLoadAfter([ContaoNewsBundle::class]),
         ];
     }
 }
