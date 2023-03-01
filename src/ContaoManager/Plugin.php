279247c8f2ae03654fd282d2fa08df4d9670b1c0
Stefan Lindecke <stefan@chektrion.de>
2023-03-01 11:03:44 +0100

    Zeugs

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -15,7 +15,7 @@ class Plugin implements BundlePluginInterface
     /**
      * {@inheritdoc}
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(ToolboxBundle::class)
